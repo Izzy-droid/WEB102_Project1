48ae9cba77b2a8e58b2a76aec4149e037b4a1e47
Izzy-droid <isdm908@gmail.com>
2025-03-01 10:23:54 -0500

    Updated some files

--- Kitty Gourmets/App.jsx
+++ Kitty Gourmets/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
      <div className="card-arrange">
        <div className="card">
-        <img src="basil.png" className="plant-img" alt="photo of a basil plant"></img>
+        <img src="basil.png" className="plant-img" alt="photo of a basil plant"/>
          <h2>{plant1}</h2>
          <p>A common food ingredient in seasoning</p>
          <a href="https://vetexplainspets.com/do-cats-like-basil/" >
@@ -30,7 +30,7 @@ const App = () => {
       </div>
 
       <div className="card">
-      <img src="Rosemary.png" className="plant-img" alt="photo of a rosemary plant"></img>
+      <img src="Rosemary.png" className="plant-img" alt="photo of a rosemary plant"/>
          <h2>{plant2}</h2>
          <p>Fun fact: They can keep fleas away</p>
          <a href="https://www.thesprucepets.com/is-rosemary-safe-for-cats-6828205">
@@ -39,7 +39,7 @@ const App = () => {
       </div>
 
       <div className="card">
-      <img src="Bamboo.png" className="plant-img" alt="photo of a bamboo plant"></img>
+      <img src="Bamboo.png" className="plant-img" alt="photo of a bamboo plant"/>
          <h2>*{plant3}</h2>
          <p>*non-toxic except lucky and heavenly bamboo</p>
          <a href="https://www.hepper.com/can-cats-eat-bamboo/">
@@ -48,7 +48,7 @@ const App = () => {
       </div>
 
       <div className="card">
-      <img src="Valerian.png" className="plant-img" alt="photo of a valerian plant"></img>
+      <img src="Valerian.png" className="plant-img" alt="photo of a valerian plant"/>
          <h2>{plant4}</h2>
          <p>A common herb that is used to relax nerves</p>
          <a href="https://www.lovecats.org/cat-food/is-it-safe-for-cats-to-eat-valerian-root/#How_Much_Is_Valerian_Safe_For_Cats">
@@ -56,7 +56,7 @@ const App = () => {
          </a>
       </div>
       <div className="card">
-      <img src="Parsley.png" className="plant-img" alt="photo of a valerian plant"></img>
+      <img src="Parsley.png" className="plant-img" alt="photo of a valerian plant"/>
          <h2>{plant5}</h2>
          <p>A common seasoning on food</p>
          <a href="https://www.catster.com/nutrition/can-cats-eat-parsley/">
@@ -64,7 +64,7 @@ const App = () => {
          </a>
       </div>
       <div className="card">
-      <img src="Silver Vine.png" className="plant-img" alt="photo of a Silver Vine plant"></img>
+      <img src="Silver Vine.png" className="plant-img" alt="photo of a Silver Vine plant"/>
          <h2>{plant6}</h2>
          <p>A herb that creates a similar effect to catnip</p>
          <a href="https://www.greatpetcare.com/wellness/silver-vine-for-cats/">
@@ -72,7 +72,7 @@ const App = () => {
          </a>
       </div>
       <div className="card">
-      <img src="Catnip.png" className="plant-img" alt="photo of a Catnip plant"></img>
+      <img src="Catnip.png" className="plant-img" alt="photo of a Catnip plant"/>
          <h2>{plant7}</h2>
          <p>The most notable herb for cats</p>
          <a href="https://cats.com/can-cats-eat-catnip">
@@ -80,7 +80,7 @@ const App = () => {
          </a>
       </div>
       <div className="card">
-      <img src="Mint.png" className="plant-img" alt="photo of a Mint plant"></img>
+      <img src="Mint.png" className="plant-img" alt="photo of a Mint plant"/>
          <h2>{plant8}</h2>
          <p>A common ingredient in food and other items</p>
          <a href="https://www.catster.com/nutrition/can-cats-eat-mint/">
@@ -88,7 +88,7 @@ const App = () => {
          </a>
       </div>
       <div className="card">
-      <img src="Thyme.png" className="plant-img" alt="photo of a Thyme plant"></img>
+      <img src="Thyme.png" className="plant-img" alt="photo of a Thyme plant"/>
          <h2>{plant9}</h2>
          <p>Used for seasoning and medicinal purposes</p>
          <a href="https://wishbonepet.com/can-cats-eat-thyme/">
@@ -96,7 +96,7 @@ const App = () => {
          </a>
       </div>
       <div className="card">
-      <img src="Lemongrass.png" className="plant-img" alt="photo of a Lemongrass plant"></img>
+      <img src="Lemongrass.png" className="plant-img" alt="photo of a Lemongrass plant"/>
          <h2>{plant10}</h2>
          <p>Fun Fact: It can be used as a mosquito repellent</p>
          <a href="https://vetexplainspets.com/is-lemongrass-toxic-to-cats/">
